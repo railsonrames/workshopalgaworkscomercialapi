7f460545391175b15f6bf6012caf1f23dbfdc63d
Railson Rames <rames.dev@outlook.com>
2019-11-16 14:06:11 -0300

    Metodo buscar oportunidade.

--- src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
+++ src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
@@ -1,11 +1,14 @@
 package com.algaworks.comercial.controller;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
+//import java.math.BigDecimal;
+//import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,9 +23,19 @@ public class OportunidadeController {
 	private OportunidadeRepository oportunidadeRepository;
 	
 	@GetMapping
-	public List<Oportunidade> listar() {
-				
+	public List<Oportunidade> listar() {				
 		return oportunidadeRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Oportunidade> buscar(@PathVariable Long id) {
+		Optional<Oportunidade> oportunidade = oportunidadeRepository.findById(id);
+		
+		if (oportunidade.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.ok(oportunidade.get());
 	}
 	
 }
