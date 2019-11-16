91c02cb34e5e0db3f09b1cdca3f9036a494c12b9
Railson Rames <rames.dev@outlook.com>
2019-11-16 13:48:12 -0300

    + retorno de listagem do banco com Spring JPA.

--- src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
+++ src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
@@ -4,25 +4,25 @@ import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.comercial.model.Oportunidade;
+import com.algaworks.comercial.repository.OportunidadeRepository;
 
 @RestController
 @RequestMapping("/oportunidades")
 public class OportunidadeController {
 
+	@Autowired
+	private OportunidadeRepository oportunidadeRepository;
+	
 	@GetMapping
 	public List<Oportunidade> listar() {
-		Oportunidade oportunidade = new Oportunidade();
-		oportunidade.setId(1L);
-		oportunidade.setDescricao("Desenvolvimento de ERP com Angular e Spring");
-		oportunidade.setNomeProspecto("Grupo Logística Brasil Show");
-		oportunidade.setValor(new BigDecimal(490000));
-		
-		return Arrays.asList(oportunidade);
+				
+		return oportunidadeRepository.findAll();
 	}
 	
 }
