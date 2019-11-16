e5cc66d3723b6f991d1122d16bb2edbe34b90c3f
Railson Rames <rames.dev@outlook.com>
2019-11-16 19:59:53 -0300

    Métodos de busca, criação e deleção de oportunidade.

--- src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
+++ src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
@@ -5,12 +5,22 @@ package com.algaworks.comercial.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.algaworks.comercial.model.Oportunidade;
 import com.algaworks.comercial.repository.OportunidadeRepository;
@@ -21,21 +31,45 @@ public class OportunidadeController {
 
 	@Autowired
 	private OportunidadeRepository oportunidadeRepository;
-	
+
 	@GetMapping
-	public List<Oportunidade> listar() {				
+	public List<Oportunidade> listar() {
 		return oportunidadeRepository.findAll();
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Oportunidade> buscar(@PathVariable Long id) {
 		Optional<Oportunidade> oportunidade = oportunidadeRepository.findById(id);
-		
+
 		if (oportunidade.isEmpty()) {
 			return ResponseEntity.notFound().build();
 		}
-		
+
 		return ResponseEntity.ok(oportunidade.get());
 	}
-	
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Oportunidade adicionar(@Valid @RequestBody Oportunidade oportunidade) {
+		Optional<Oportunidade> oportunidadeExistente = oportunidadeRepository
+				.findByDescricaoAndNomeProspecto(oportunidade.getDescricao(), oportunidade.getNomeProspecto());
+		
+		if (oportunidadeExistente.isPresent()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Já existe uma oportunidade para este prospecto com a mesma descrição");
+		}
+		return oportunidadeRepository.save(oportunidade);
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void excluir(@PathVariable Long id) {
+		Boolean oportunidadeExistente = oportunidadeRepository.existsById(id);
+		
+		if (!oportunidadeExistente) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não existe oportunidade cadastrada com o id informado.");
+		}
+		
+		oportunidadeRepository.deleteById(id);
+	}
+
 }
--- src/main/java/com/algaworks/comercial/model/Oportunidade.java
+++ src/main/java/com/algaworks/comercial/model/Oportunidade.java
@@ -5,17 +5,26 @@ import java.math.BigDecimal;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Oportunidade {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotEmpty
+	@Size(max = 80)
 	@Column(name = "nome_prospecto")
 	private String nomeProspecto;
+	@NotEmpty
+	@Size(max = 200)
 	private String descricao;	
+	@Min(0)
 	private BigDecimal valor;
 	
 	public Long getId() {
